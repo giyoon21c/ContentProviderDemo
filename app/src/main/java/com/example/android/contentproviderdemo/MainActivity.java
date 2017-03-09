6eabf9d00e1fc6383ef2d2bc3e9ae72d363e9c36
Your Name <giyoon21c@gmail.com>
2017-03-08 16:45:23 -0800

    ContentProvider in progress

--- app/src/main/java/com/example/android/contentproviderdemo/MainActivity.java
+++ app/src/main/java/com/example/android/contentproviderdemo/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.android.contentproviderdemo;
 
+import android.app.LoaderManager;
 import android.content.ContentResolver;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "ContentProviderDemo";
 
     private TextView textViewQueryResult;
+    private Button buttonLoadData;
+
+    private boolean firstTimeLoaded = false;
 
     private String[] mColumnProjection = new String[] {
         ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
@@ -25,31 +36,60 @@ public class MainActivity extends AppCompatActivity {
 
     private String mOrderBy = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonLoadData = (Button) findViewById(R.id.buttonLoadData);
+
+        // LoaderManager is initialized when a button is clicked
+        buttonLoadData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "button pressed");
+                getLoaderManager().initLoader(1, null, MainActivity.this); // this creates onCreateLoader
+            }
+        });
+
         textViewQueryResult = (TextView) findViewById(R.id.textViewQueryResult);
 
-        ContentResolver contentResolver = getContentResolver();
+    }
 
-        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
-                mColumnProjection,
-                mSelectionClause,
-                mSelectionArguments,
-                null);
+    // three messages that override LoaderCallbacks<Cursor>
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        if (i == 1) {
+            return new CursorLoader(MainActivity.this, ContactsContract.Contacts.CONTENT_URI,
+                    mColumnProjection,
+                    null,
+                    null,
+                    null);
+        }
+        return null;
+    }
 
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         if ((cursor != null) && (cursor.getCount() > 0)) {
             StringBuilder stringBuilderQueryResult= new StringBuilder("");
             while (cursor.moveToNext()) {
                 stringBuilderQueryResult.append(cursor.getString(0) + ", " +
-                                                cursor.getString(1) + ", " +
-                                                cursor.getString(2) + "\n");
+                        cursor.getString(1) + ", " +
+                        cursor.getString(2) + "\n");
             }
             textViewQueryResult.setText(stringBuilderQueryResult.toString());
         } else {
             textViewQueryResult.setText("No contact in device");
         }
+
     }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
 }
